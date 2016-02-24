6a0db299b63c39560f687ee5e4d1d1098cdfc26c
andreygalassi <andreygalassi@gmail.com>
2016-02-24 20:45:19 -0300

    classe utilitario para conversão de cada linha do arquivo e teste

---
+++ pre-dojo/src/main/java/br/agrego/predojo/util/TradutorUtil.java
@@ -0,0 +1,64 @@
+package br.agrego.predojo.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import br.agrego.predojo.model.Arma;
+import br.agrego.predojo.model.Jogador;
+
+public class TradutorUtil {
+	
+	private TradutorUtil() {
+		// TODO Auto-generated constructor stub
+	}
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+	public static Date getData(String registro) {
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(registro).useDelimiter(" - ");
+		Date time = null;
+		try {
+			time = sdf.parse(sc.next());
+		} catch (ParseException e) {
+			System.out.println("Erro ao ler a data do log: " + registro);
+		}
+		return time;
+	}
+	public static String getCodigoPartida(String registro) {
+		String dado = registro.split(" - ")[1];
+		dado = dado.replace("New match", "");
+		dado = dado.replace("has started", "");
+		return dado.trim();
+	}
+	public static Jogador getJogadorMortoPeloMundo(String registro) {
+		String dado = registro.split(" - ")[1];
+		dado = dado.replace("<WORLD> killed", "");
+		dado = dado.substring(0, dado.indexOf("by"));
+		
+		return new Jogador(dado.trim());
+	}
+	public static Jogador getJogadorAssassino(String registro) {
+		String dado = registro.split(" - ")[1];
+		dado = dado.substring(0, dado.indexOf("killed"));
+
+		return new Jogador(dado.trim());
+	}
+	public static Jogador getJogadorMorto(String registro) {
+		String dado = registro.split(" - ")[1];
+		dado = dado.substring(dado.indexOf("killed")+"killed".length(), dado.indexOf("using"));
+
+		return new Jogador(dado.trim());
+	}
+	public static Arma getArma(String registro) {
+		String dado = registro.split(" - ")[1];
+		dado = dado.substring(dado.indexOf("using")+"using".length());
+		
+		return new Arma(dado.trim());
+	}
+	public static Boolean getFimDaPartida(String registro) {
+		String dado = registro.split(" - ")[1];
+		return dado.contains("has ended");
+	}
+}
---
+++ pre-dojo/src/test/java/br/agrego/predojo/util/TradutorTest.java
@@ -0,0 +1,72 @@
+package br.agrego.predojo.util;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.agrego.predojo.model.Arma;
+import br.agrego.predojo.model.Jogador;
+
+public class TradutorTest {
+
+	private final static String LOG1 = "23/04/2013 15:34:22 - New match 11348965 has started";
+	private final static String LOG2 = "23/04/2013 15:36:04 - Roman killed Nick using M16";
+	private final static String LOG3 = "23/04/2013 15:36:33 - <WORLD> killed Nick by DROWN";
+	private final static String LOG4 = "23/04/2013 15:39:22 - Match 11348965 has ended";
+	
+	
+	@Test
+	public void deveIdentificarAData() {
+		Date data = TradutorUtil.getData(LOG1);
+		Calendar gc = GregorianCalendar.getInstance();
+		gc.set(2013, 3, 23, 15, 34, 22);
+		
+		assertEquals(gc.getTime().toString(), data.toString());
+	}
+	
+	@Test
+	public void deveIdentificarOCodigoDaPartida() {
+		String codigo = TradutorUtil.getCodigoPartida(LOG1);
+		
+		assertEquals("11348965", codigo);
+	}
+	
+	@Test
+	public void deveIdentificarOAssassino() {
+		Jogador assassino = TradutorUtil.getJogadorAssassino(LOG2);
+		Jogador jEsperado = new Jogador("Roman");
+		assertEquals(jEsperado, assassino);
+	}
+	
+	@Test
+	public void deveIdentificarOMorto() {
+		Jogador morto = TradutorUtil.getJogadorMorto(LOG2);
+		Jogador jEsperado = new Jogador("Nick");
+		assertEquals(jEsperado, morto);
+	}
+	
+	@Test
+	public void deveIdentificarAArma() {
+		Arma arma = TradutorUtil.getArma(LOG2);
+		Arma aEsperada = new Arma("M16");
+		assertEquals(aEsperada, arma);
+	}
+	
+	@Test
+	public void deveIdentificarMortoPeloMundo() {
+		Jogador morto = TradutorUtil.getJogadorMortoPeloMundo(LOG3);
+		Jogador jEsperado = new Jogador("Nick");
+		assertEquals(jEsperado, morto);
+	}
+	
+	@Test
+	public void deveIdentificarFimDaPartida() {
+		Boolean fim = TradutorUtil.getFimDaPartida(LOG4);
+		Assert.assertTrue(fim);
+	}
+}
